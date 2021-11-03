21894a7e0b394d4dcfdac1fbdfaa7f718e3ea34f
giorgirogava <giorgirogava01ai@gmail.com>
2021-11-03 19:57:50 +0400

    Initial Commit

---
+++ homework19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework19
+//
+//  Created by IMAC on 03.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
